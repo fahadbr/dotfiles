c7a23da10171fd83096b510d00a0e474e21572da
Fahad Riaz <fahadbr@gmail.com>
2018-09-13 10:51:42 -0400

    updating caps lock indicator

---
+++ scripts/capslock-notify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if $( xset -q | grep 'LED mask:  00000001' &>/dev/null ); then
+	dunstify -a '' -t 120000 -r 2596 -u critical 'CAPS LOCK ON'
+else
+	dunstify -a '' -t 750 -r 2596 -u normal 'caps lock off'
+fi
+
+# notify i3blocks
+pkill -SIGRTMIN+11 i3blocks
+
