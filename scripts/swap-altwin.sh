a5db14306db52464baa592e85ff052a1dd4c719a
Fahad Riaz <fahadbr@gmail.com>
2018-11-02 13:06:18 -0400

    swap only for logitech

--- scripts/swap-altwin.sh
+++ scripts/swap-altwin.sh
@@ -1,1 +1,4 @@
-setxkbmap -option 'altwin:swap_lalt_lwin'
+#!/bin/bash
+
+id=$(xinput list | grep 'Logitech K811 Keyboard' | sed -En 's/.*id=([0-9]+).*/\1/p')
+setxkbmap -device $id -option 'altwin:swap_lalt_lwin'
