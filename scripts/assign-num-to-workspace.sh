da84365538be07f49397d9796c8392400fd270e3
Fahad Riaz <fahadbr@gmail.com>
2019-02-27 20:46:21 -0500

    added ability to remove number if target num is already assigned in assign-num-to-workspace script

--- scripts/assign-num-to-workspace.sh
+++ scripts/assign-num-to-workspace.sh
@@ -9,24 +9,47 @@ if [[ -z $1 ]]; then
 	exit 1
 fi
 
+# renameWs attempts to rename a workspace by assigning (or removing) a number
+# possible outcomes are:
+#   OldWsName   Number   Result
+#   "1"	     	  "2"  ->  "2"
+#   "1: www"    "2"  ->  "2: www"
+#   "www"       "2"  ->  "2: www"
+#   "www"       ""   ->  "www"
+#   "1: www"    "1"  ->  "www"
+#   "1: www"    ""   ->  "www"
+#   "1"         ""   ->  "$(new name from prompt)"
 function renameWs() {
 	local oldWsName=$1
-	local numberOfFocused=$2
-	local newNameForWs=$numberOfFocused
+	local newNumForWs=$2
+	local newNameForWs=$newNumForWs
 
+	# if old workspace name was only numeric
 	if echo $oldWsName | grep -E '^[0-9]+$' &>/dev/null; then
-		if [[ -z $numberOfFocused ]]; then
-			# if the old workspace name was only a number then prompt for a new name for that workspace
+		# and there is no number for the new name
+		if [[ -z $newNumForWs ]]; then
+			# then prompt for a new name for that workspace
 			newNameForWs=$(rofi -l 0 -dmenu -P "enter name for workspace \"$oldWsName\"")
+		# if the newNumForWs is the same as the oldWsName name
+		elif [[ $oldWsName -eq $newNumForWs ]]; then
+			# then exit and do nothing
+			return
 		fi
 	else
-		# otherwise proceed to remove the number from the old ws
+		# parse the number and name component out of the old ws name
+		oldWsNum=$(echo $oldWsName | sed -En 's/(^[0-9]+)(:?.*)/\1/p')
 		oldWsNameStripped=$(echo $oldWsName | sed -E 's/(^[0-9]+: )(.*)/\2/')
 
-		if [[ -z $numberOfFocused ]]; then
+		# if there is no new number to assign then just use the name component
+		if [[ -z $newNumForWs ]]; then
 			newNameForWs=$oldWsNameStripped
+		# if the new number is the same as the old number
+		# the action is to remove the number and only use the name component
+		elif [[ $newNumForWs -eq $oldWsNum ]]; then
+			newNameForWs=$oldWsNameStripped
+		# otherwise prepend the new number to the name component
 		else
-			newNameForWs="$numberOfFocused: $oldWsNameStripped"
+			newNameForWs="$newNumForWs: $oldWsNameStripped"
 		fi
 	fi
 
@@ -46,30 +69,30 @@ numberToAssign=$1
 workspaces=$(i3-msg -t get_workspaces)
 
 # get the currently focused workspace
-focused=$(echo $workspaces | jq -r '.[] | select(.focused == true).name')
+focusedWs=$(echo $workspaces | jq -r '.[] | select(.focused == true).name')
 
 #	get the number of the currently focused ws if there is one
 # we will use this to swap numbers with the ws that has the number we want
-numberOfFocused=$(echo $focused | sed -En 's/(^[0-9]+)(:?.*)/\1/p')
+numberOfFocused=$(echo $focusedWs | sed -En 's/(^[0-9]+)(:?.*)/\1/p')
 
-if [[ "$numberToAssign" == "$numberOfFocused" ]]; then
-	exit 0
-fi
 
 # if focused is just a number then the new name will just be the numberToAssign
-if echo $focused | grep -E '^[0-9]+$' &>/dev/null; then
+if echo $focusedWs | grep -E '^[0-9]+$' &>/dev/null; then
+	# we need to temporarily rename the workspace in order
+	# to avoid conflicts (e.g. when swapping 6 & 7, 6 can't be renamed to 7
+	# while a workspace is occupying 7)
 	tmpName="99"
-	i3-msg "rename workspace \"$focused\" to \"$tmpName\""
-	focused=$tmpName
+	i3-msg "rename workspace \"$focusedWs\" to \"$tmpName\""
+	focusedWs=$tmpName
 fi
 
 oldWs=$(echo $workspaces | jq -r ".[] | select(.num == $numberToAssign).name")
 # if a workspace already has the number we need, we go into the flow below
-if [[ $oldWs ]]; then
+if [[ $oldWs ]] && [[ "$oldWs" != "$focusedWs" ]]; then
 	renameWs "$oldWs" $numberOfFocused
 fi
 
 
-# rename the focused workspace, placing the number in front of it
-renameWs "$focused" $numberToAssign
+# rename the focusedWs workspace, placing the number in front of it
+renameWs "$focusedWs" $numberToAssign
 
