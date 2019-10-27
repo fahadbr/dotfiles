40730705e151ec51b1a2839592dfe8fd21c69292
Fahad Riaz <fahadbr@gmail.com>
2019-10-27 15:00:16 -0400

    updating bww for rofi use

--- scripts/rofi/pass-rofi.sh
+++ scripts/rofi/pass-rofi.sh
@@ -2,6 +2,7 @@
 
 set -eo pipefail
 shopt -s nullglob globstar
+bww=$HOME/.dotfiles/scripts/bww
 
 typeit=0
 if [[ $1 == "--type" ]]; then
@@ -28,12 +29,25 @@ function dopass {
 
 function dolpass {
 	selected=$(lpass ls --sync=no | grep id | rofi -dmenu -p 'lpass')
+	[[ -n $selected ]] || exit
+
 	lpassID=$(echo $selected | sed -E 's/.*id: ([0-9]+)]/\1/')
 	lpass show --sync=no --password $lpassID 2>&1 | { IFS= read -r pass; printf %s "$pass"; } | xdotool type --clearmodifiers --file -
 }
 
+function dobw {
+	bww unlock
+	selected=$($bww list | rofi -dmenu -p 'bitwarden')
+	[[ -n $selected ]] || exit
+
+	bwID=$(echo $selected | cut -d ':' -f 2)
+	$bww show $bwID | { IFS= read -r pass; printf %s "$pass"; } | xdotool type --clearmodifiers --file -
+}
+
 if which lpass &>/dev/null ; then
 	dolpass
+elif [[ -f $bww ]]; then
+	dobw
 else
 	dopass
 fi
