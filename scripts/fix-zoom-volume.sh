37eaf36cf7a3e7a2fd499e79164b641eed6d3d56
Fahad Riaz <fahadbr@gmail.com>
2020-04-30 16:00:01 -0400

    auto commit

--- scripts/fix-zoom-volume.sh
+++ scripts/fix-zoom-volume.sh
@@ -3,5 +3,7 @@
 set -e
 
 id=$(pulsemixer --list-sinks | grep ZOOM | sed 's/.*\(ID: \)\([a-z0-9\-]\+\).*/\2/')
+
+read left_vol right_vol <<<$(pulsemixer --get-volume --id $id)
 pulsemixer --set-volume 100 --id $id
-notify-send -a $0 "Set Zoom Volume to 100%"
+notify-send -a $0 "Changed Zoom Volume from ${left_vol}% to 100%"
