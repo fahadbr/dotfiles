1ac830def21ad2ae42750fdfaf2d4325a8857b60
Fahad Riaz <fahadbr@gmail.com>
2020-05-05 16:00:01 -0400

    auto commit

--- scripts/home-monitor.sh
+++ scripts/home-monitor.sh
@@ -11,7 +11,8 @@ fi
 if [[ "$MACHINE" == "home" ]]; then
 	xrandr --output $display --primary --auto --scale .6x.6 --right-of eDP1
 else
-	xrandr --output $display --primary --auto --scale .5x.5 --left-of eDP1
+	#xrandr --output $display --primary --mode 1920x1080 --left-of eDP1
+	xrandr --output $display --primary --auto --scale .5x.5 --output eDP1 --pos 1920x0
 fi
 
 systemctl --user start powerman@ac.service
