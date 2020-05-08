e83852f47eeffdfa770e7a208f15ea8762431002
Fahad Riaz <fahadbr@gmail.com>
2020-05-08 13:13:58 -0400

    auto commit

--- scripts/autocommitter.sh
+++ scripts/autocommitter.sh
@@ -2,24 +2,18 @@
 
 set -u
 
-senderr() {
-	if [ "$1" ]
-	then
-		notify-send -a "$0" "$AUTO_COMMIT_REPO" "failed to auto commit: $1"
-		exit 1
-	fi
-}
 
 do_commit_and_push() {
 	cd $AUTO_COMMIT_REPO
 	git pull && git commit -am 'auto commit' && git push
 }
 
-tmpfile=$(mktemp /tmp/autocommiterror.XXXXXXX.txt)
-trap "rm -f $tmpfile" EXIT
-do_commit_and_push 2>$tmpfile >/dev/null
-senderr "$(cat $tmpfile)"
-
+if ! res=$(do_commit_and_push 2>&1 >/dev/null); then
+	if [ "$res" ]; then
+		notify-send -a "$0" "$AUTO_COMMIT_REPO" "failed to auto commit: $res"
+		exit 1
+	fi
+fi
 
 # send notification if there's any untracked files
 untracked=$(git status -uall -s | grep '^??' | wc -l)
