a8073b04287d23b752c8f0339d4efd3c78c9efaa
Fahad Riaz <fahadbr@gmail.com>
2020-05-31 17:14:31 -0400

    updated xkbdrate script to include planck

--- scripts/set-xkbdrate.sh
+++ scripts/set-xkbdrate.sh
@@ -1,4 +1,11 @@
+
+reset_keyboard() {
+	keyboard=$1
+	id=$(xinput list | grep "$1" | grep 'keyboard' | grep -v 'Control' | sort | head -n1 | sed -En 's/.*id=([0-9]+).*/\1/p')
+	setxkbmap -device $id -option ''
+}
+
 xset r rate 200 45
 setxkbmap -option 'caps:swapescape'
-id=$(xinput list | grep "Keebio \"Fahad's Iris Keyboard\"" | grep 'keyboard' | grep -v 'Control' | sed -En 's/.*id=([0-9]+).*/\1/p')
-setxkbmap -device $id -option ''
+reset_keyboard "Keebio \"Fahad's Iris Keyboard\""
+reset_keyboard "OLKB \"Fahad's Planck Keyboard\""
