9358b6572dec2ab20205979ffbbc5083f2967019
Fahad Riaz <fahadbr@gmail.com>
2020-06-15 12:00:04 -0400

    auto commit

--- scripts/start-compositor.sh
+++ scripts/start-compositor.sh
@@ -2,6 +2,8 @@
 
 if which picom &>/dev/null; then
 	picom --config /etc/xdg/picom.conf
+elif which compton &>/dev/null; then
+	compton --config ~/.dotfiles/compton.conf
 elif which xcompmgr &>/dev/null; then
 	xcompmgr -c -l0 -t0 -r0 -o.00
 fi
