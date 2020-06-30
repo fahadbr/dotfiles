7f036d5d33d829f47d581358fa3bb9f537dafa79
Fahad Riaz <fahadbr@gmail.com>
2020-06-30 14:00:05 -0400

    auto commit

--- scripts/laptop-only.sh
+++ scripts/laptop-only.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+	xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --scale 1x1 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
 systemctl --user start powerman@bat.service
 pactl set-card-profile 0 output:analog-stereo
