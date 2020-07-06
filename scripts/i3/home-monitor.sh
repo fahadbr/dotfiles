6003b9fe08aa9575755529fd885ef5dfcfb4b07b
Fahad Riaz <fahadbr@gmail.com>
2020-07-06 17:58:40 -0400

    home monitor change

--- scripts/i3/home-monitor.sh
+++ scripts/i3/home-monitor.sh
@@ -8,7 +8,8 @@ else
 	display=$(xrandr | grep '\bconnected' | grep -v 'eDP1' | cut -d ' ' -f 1)
 fi
 
-xrandr --output $display --primary --auto --scale .5x.5 --output eDP1 --pos 1920x0
+#xrandr --output $display --primary --auto --scale .5x.5 --output eDP1 --pos 1920x0
+xrandr --output $display --primary --auto --output eDP1 --right-of $display
 
 systemctl --user start powerman@ac.service
 $HOME/scripts/set-xkbdrate.sh
