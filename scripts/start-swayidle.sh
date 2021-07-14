2e863e8591ad5b2e44e3eade16488f7a9464e4b8
Fahad Riaz <fahadbr@gmail.com>
2021-07-14 11:32:57 -0400

    minor updates

--- scripts/start-swayidle.sh
+++ scripts/start-swayidle.sh
@@ -5,11 +5,11 @@ lockcmd='swaylock -f -i ~/.config/lockscreen.png -s tile'
 if [[ "$1" == "lock" ]]; then
 	$lockcmd
 elif [[ "$1" == "ac" ]]; then
-	swayidle -w timeout 120 'dimmer dim' resume 'dimmer restore' \
+	swayidle -w timeout 120 "$HOME/.local/bin/dimmer dim" resume "$HOME/.local/bin/dimmer restore" \
 		timeout 600 "$lockcmd" \
 		before-sleep "$lockcmd"
 else
-	swayidle -w timeout 60 'dimmer dim' resume 'dimmer restore' \
+	swayidle -w timeout 60 "$HOME/.local/bin/dimmer dim" resume "$HOME/.local/bin/dimmer restore" \
 		timeout 120 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
 		timeout 300 "$lockcmd" \
 		timeout 600 'systemctl suspend' \
