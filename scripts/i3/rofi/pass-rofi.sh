12fb8eea55a096e8ca74bd3860b6c4de4351e4b9
Fahad Riaz <fahadbr@gmail.com>
2021-08-24 22:08:16 -0400

    i3 updates

--- scripts/i3/rofi/pass-rofi.sh
+++ scripts/i3/rofi/pass-rofi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-set -exo pipefail
-#shopt -s nullglob globstar
+set -eo pipefail
+shopt -s nullglob globstar
 bww=$HOME/.dotfiles/scripts/bww
 
 dotype() {
@@ -11,6 +11,28 @@ dotype() {
 	else
 		echo $1 | xdotool type --clearmodifiers --file -
 	fi
+}
+
+
+function dopass {
+	prefix=${PASSWORD_STORE_DIR-~/.password-store}
+	password_files=( "$prefix"/**/*.gpg )
+	password_files=( "${password_files[@]#"$prefix"/}" )
+	password_files=( "${password_files[@]%.gpg}" )
+
+	password=$(printf '%s\n' "${password_files[@]}" | rofi -dmenu -p "pass")
+
+	[[ -n $password ]] || exit
+
+	dotype "$(pass show "$password" | { IFS= read -r pass; printf %s "$pass"; })"
+}
+
+function dolpass {
+	selected=$(lpass ls --sync=no | grep id | rofi -dmenu -p 'lpass')
+	[[ -n $selected ]] || exit
+
+	lpassID=$(echo $selected | sed -E 's/.*id: ([0-9]+)]/\1/')
+	dotype "$(lpass show --sync=no --password $lpassID 2>&1 | { IFS= read -r pass; printf %s "$pass"; })"
 }
 
 dobw() {
@@ -23,4 +45,17 @@ dobw() {
 	dotype $secret
 }
 
-dobw
+case "$1" in
+	lastpass) dolpass ;;
+	bitwarden) dobw ;;
+	pass) dopass ;;
+	*)
+		if which lpass &>/dev/null ; then
+			dolpass
+		elif which bw &> /dev/null ; then
+			dobw
+		else
+			dopass
+		fi
+		;;
+esac
