8451fedac487b61596b6aa8b167afa22e18ae45f
Fahad Riaz <fahadbr@gmail.com>
2021-09-27 10:13:47 -0400

    fine tuning

--- scripts/i3/home-monitor.sh
+++ scripts/i3/home-monitor.sh
@@ -12,8 +12,11 @@ else
 	extdisplay=$(xrandr | grep '\bconnected' | grep -v 'eDP' | cut -d ' ' -f 1)
 	localdisplay=$(xrandr | grep '\bconnected' | grep 'eDP' | cut -d ' ' -f 1)
 
-	xrandr --output $extdisplay --primary --mode 3840x1200 --rate 120.00 \
-		--output eDP-1 --off
+	if [[ $localdisplay ]]; then
+		xrandr --output $extdisplay --primary --mode 3840x1200 --rate 120.00 --left-of $localdisplay
+	else
+		xrandr --output $extdisplay --primary --mode 3840x1200 --rate 120.00
+	fi
 
 
 	$HOME/.dotfiles/scripts/set-xkbdrate.sh
