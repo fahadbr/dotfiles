e3cdccb1fa1895c36eec05ba50c715f59722e9b8
Fahad Riaz <fahadriaz@fb.com>
2022-05-31 10:51:06 -0400

    stuff for mac

---
+++ setup/macos-config.sh
@@ -0,0 +1,9 @@
+
+# Note: Must re-login to session after changing these
+
+# this enables dragging windows with cmd+ctrl+click
+# to disable run:
+# defaults delete -g NSWindowShouldDragOnGesture
+defaults write -g NSWindowShouldDragOnGesture -bool true
+
+
