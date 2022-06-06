8b44d55aa8ae67952a772f867df7b725587c2bb4
Fahad Riaz <fahadriaz@fb.com>
2022-06-06 12:41:56 -0400

    mac setup stuff

--- setup/macos-config.sh
+++ setup/macos-config.sh
@@ -6,4 +6,10 @@
 # defaults delete -g NSWindowShouldDragOnGesture
 defaults write -g NSWindowShouldDragOnGesture -bool true
 
-
+# set up touchid for authenticating sudo
+if ! cat /etc/pam.d/sudo | grep -q pam_tid.so; then
+	header=$(cat /etc/pam.d/sudo | head -n 1)
+	body=$(cat /etc/pam.d/sudo | tail -n +2)
+	new_contents="$header\nauth       sufficient     pam_tid.so\n$body"
+	echo "$new_contents" | sudo tee /etc/pam.d/sudo
+fi
