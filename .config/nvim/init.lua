60683360d1227ce49b46f68ed4165db88da2b664
Fahad Riaz <fahadbr@gmail.com>
2024-03-25 12:05:56 -0400

    converting airline cfg to lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -210,58 +210,44 @@ local vim_airline = {
   'vim-airline/vim-airline',
   dependencies = { 'vim-airline/vim-airline-themes' },
   init = function()
-    vim.cmd([[
-      let g:airline_powerline_fonts = 1
-      let g:airline_theme = 'night_owl'
+    vim.g.airline_powerline_fonts = 1
+    vim.g.airline_theme = 'night_owl'
 
-      if !exists('g:airline_symbols')
-        let g:airline_symbols = {}
-      endif
+    local airline_symbols = vim.g.airline_symbols
+    if airline_symbols == nil then
+      airline_symbols = {}
+    end
+    airline_symbols.paste = '∥'
+    airline_symbols.whitespace = 'Ξ'
+    airline_symbols.branch = ''
+    airline_symbols.readonly = ''
+    airline_symbols.linenr = ''
+    vim.g.airline_symbols = airline_symbols
+    vim.g.airline_left_sep = ''
+    vim.g.airline_left_alt_sep = ''
+    vim.g.airline_right_sep = ''
+    vim.g.airline_right_alt_sep = ''
+    vim.g.airline_highlighting_cache = 1
 
-      " unicode symbols
-      let g:airline_left_sep = '»'
-      let g:airline_left_sep = '▶'
-      let g:airline_right_sep = '«'
-      let g:airline_right_sep = '◀'
-      let g:airline_symbols.linenr = '␊'
-      let g:airline_symbols.linenr = '␤'
-      let g:airline_symbols.linenr = '¶'
-      let g:airline_symbols.branch = '⎇'
-      let g:airline_symbols.paste = 'ρ'
-      let g:airline_symbols.paste = 'Þ'
-      let g:airline_symbols.paste = '∥'
-      let g:airline_symbols.whitespace = 'Ξ'
+    -- airline tabline
+    vim.g['airline#extensions#tabline#enabled'] = 1
+    vim.g['airline#extensions#tabline#tab_nr_type'] = 1
+    vim.g['airline#extensions#tabline#buffer_idx_mode'] = 1
+    vim.g['airline#extensions#tabline#formatter'] = 'unique_tail_improved'
+    vim.g['airline#extensions#tabline#fnamemod'] = ':t'
+    vim.g['airline#extensions#tabline#fnamecollapse'] = 1
 
-      " airline symbols
-      let g:airline_left_sep = ''
-      let g:airline_left_alt_sep = ''
-      let g:airline_right_sep = ''
-      let g:airline_right_alt_sep = ''
-      let g:airline_symbols.branch = ''
-      let g:airline_symbols.readonly = ''
-      let g:airline_symbols.linenr = ''
-      let g:airline_highlighting_cache = 1
-
-      " airline tabline
-      let g:airline#extensions#tabline#enabled = 1
-      let g:airline#extensions#tabline#tab_nr_type = 1
-      let g:airline#extensions#tabline#buffer_idx_mode = 1
-      let g:airline#extensions#tabline#formatter = 'unique_tail_improved'
-      let g:airline#extensions#tabline#fnamemod = ':t'
-      let g:airline#extensions#tabline#fnamecollapse = 1
-
-      nmap <leader>1 <Plug>AirlineSelectTab1
-      nmap <leader>2 <Plug>AirlineSelectTab2
-      nmap <leader>3 <Plug>AirlineSelectTab3
-      nmap <leader>4 <Plug>AirlineSelectTab4
-      nmap <leader>5 <Plug>AirlineSelectTab5
-      nmap <leader>6 <Plug>AirlineSelectTab6
-      nmap <leader>7 <Plug>AirlineSelectTab7
-      nmap <leader>8 <Plug>AirlineSelectTab8
-      nmap <leader>9 <Plug>AirlineSelectTab9
-      nmap <M-l> <Plug>AirlineSelectNextTab
-      nmap <M-h> <Plug>AirlineSelectPrevTab
-    ]])
+    nmap('<leader>1', '<Plug>AirlineSelectTab1')
+    nmap('<leader>2', '<Plug>AirlineSelectTab2')
+    nmap('<leader>3', '<Plug>AirlineSelectTab3')
+    nmap('<leader>4', '<Plug>AirlineSelectTab4')
+    nmap('<leader>5', '<Plug>AirlineSelectTab5')
+    nmap('<leader>6', '<Plug>AirlineSelectTab6')
+    nmap('<leader>7', '<Plug>AirlineSelectTab7')
+    nmap('<leader>8', '<Plug>AirlineSelectTab8')
+    nmap('<leader>9', '<Plug>AirlineSelectTab9')
+    nmap('<M-l>', '<Plug>AirlineSelectNextTab')
+    nmap('<M-h>', '<Plug>AirlineSelectPrevTab')
   end,
 }
 
