234b65bb47825f371218cfbf406543f2edef76f7
Fahad Riaz <fahadbr@gmail.com>
2024-04-02 09:34:33 -0400

    session config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -318,8 +318,41 @@ local persisted_plugin_spec = {
   "olimorris/persisted.nvim",
   lazy = false, -- make sure the plugin is always loaded at startup
   config = function()
-    require('persisted').setup({})
+    local persisted = require('persisted')
+    persisted.setup({
+      telescope = {
+        reset_prompt = true,
+        mappings = {
+          change_branch = '<c-b>',
+          copy_session = '<m-c>',
+          delete_session = '<c-d>',
+        }
+      }
+    })
+
     require("telescope").load_extension("persisted")
+
+    local augroup = vim.api.nvim_create_augroup("PersistedHooks", {})
+    autocmd({ "User" }, {
+      pattern = "PersistedTelescopeLoadPre",
+      group = augroup,
+      callback = function()
+        -- save the currently loaded session using the global variable
+        persisted.save({ session = vim.g.persisted_loaded_session })
+
+        -- delete all open buffers
+        vim.api.nvim_input("<ESC>:%bd!<CR>")
+      end,
+    })
+    autocmd({ 'User' }, {
+      pattern = 'PersistedLoadPost',
+      group = augroup,
+      callback = function(session)
+        -- set the process title to the directory name when a session is loaded
+        local dirname = vim.fn.fnamemodify(session.data.dir_path, ':t')
+        vim.o.titlestring = dirname
+      end,
+    })
   end
 }
 -- }}}
