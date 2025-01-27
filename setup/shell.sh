d37e47453acd6888d15692afa9a4ca1bfb1b82cc
Fahad Riaz <fahadbr@gmail.com>
2025-01-27 11:36:56 -0500

    removing omz installation

--- setup/shell.sh
+++ setup/shell.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
 set -ex
-# download oh my zsh
-if [[ ! -d $HOME/.oh-my-zsh ]]; then
-	echo "Downloading oh-my-zsh"
-	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-
-pushd $HOME/.oh-my-zsh/custom/plugins/
-
-# clone zsh autosuggestions
-if [[ ! -d ./zsh-autosuggestions ]]; then
-	echo "installing zsh-autosuggestions"
-	git clone https://github.com/zsh-users/zsh-autosuggestions.git
-fi
-
-# clone zsh highlighting
-if [[ ! -d ./zsh-syntax-highlighting ]]; then
-	echo "installing zsh-syntax-highlighting"
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-fi
-
-popd
-
 
 # clone and install fzf
 if [[ ! -d $HOME/.fzf ]]; then
