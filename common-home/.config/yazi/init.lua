b1d34c1c27145321076657374620653874fec2c2
Fahad Riaz <fahadbr@gmail.com>
2025-01-27 12:42:39 -0500

    yazi config

---
+++ common-home/.config/yazi/init.lua
@@ -0,0 +1,8 @@
+
+-- adds username and hostname to the headers
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ""
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
