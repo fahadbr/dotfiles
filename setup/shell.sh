c85f3555d8bf084d149a41167db666628ec302fb
Fahad Riaz <fahadbr@gmail.com>
2025-03-03 15:40:40 -0500

    removing tmux from shell setup

--- setup/shell.sh
+++ setup/shell.sh
@@ -9,12 +9,6 @@ if [[ ! -d $HOME/.fzf ]]; then
 	yes | ~/.fzf/install
 fi
 
-tmux_plugins_dir="${HOME}/.tmux/plugins"
-if [[ ! -d ${tmux_plugins_dir} ]]; then
-	echo "installing tpm (tmux plugin manager)"
-	git clone https://github.com/tmux-plugins/tpm ${tmux_plugins_dir}/tpm && \
-		${tmux_plugins_dir}/tpm/bin/install_plugins
-fi
 
 
 
