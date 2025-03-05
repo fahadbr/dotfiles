8b7c9fee258babc63b0c62a0308bf992a195cde9
Fahad Riaz <fahadbr@gmail.com>
2025-03-05 11:41:12 -0500

    kitty and aerospace updates

--- macos-home/.config/aerospace/scratch.sh
+++ macos-home/.config/aerospace/scratch.sh
@@ -3,42 +3,31 @@
 APP_ID=$1
 APP_NAME=$2
 
+IFS='|' read windowid workspace <<<"$(aerospace list-windows --monitor all --app-id $APP_ID --format '%{window-id}|%{workspace}' | head -n 1)"
+current_window="$(aerospace list-windows --focused --format '%{window-id}')"
+
 summon_app() {
   current_workspace=$(aerospace list-workspaces --focused)
-  app_window_id=$(aerospace list-windows --monitor all --app-id "$APP_ID" --format "%{window-id}" | head -n 1)
-  aerospace move-node-to-workspace --window-id $app_window_id $current_workspace
-  aerospace focus --window-id $app_window_id
+  aerospace move-node-to-workspace --window-id $windowid $current_workspace
+  aerospace focus --window-id $windowid
   aerospace move-mouse window-lazy-center
 }
 
-app_closed() {
-  if [ "$(aerospace list-windows --monitor all --app-id $APP_ID)" == "" ]; then
-    true
-  else
-    false
-  fi
-}
-
-app_focused() {
-  if [ "$(aerospace list-windows --focused --format "%{app-bundle-id}")" == "$APP_ID" ]; then
-    true
-  else
-    false
-  fi
-}
-
 dismiss_app_to_scratchpad() {
-  aerospace layout floating
   aerospace move-node-to-workspace scratch
 }
 
-if app_closed; then
+# app not open
+if [[ -z $windowid ]]; then
   open -a $APP_NAME
-  sleep 0.5
+  #sleep 0.5
 else
-  if app_focused; then
+  if [[ $windowid == $current_window ]]; then
     dismiss_app_to_scratchpad
+  elif [[ $workspace != 'scratch' ]]; then
+    aerospace focus --window-id $windowid
   else
+    # app is in scratch workspace so summon it
     summon_app
   fi
 fi
