a21bc143a69bfa5af9213ea8c06b98ed2fffd499
Fahad Riaz <fahadbr@gmail.com>
2025-03-10 14:21:59 -0400

    treesitter object

--- common-home/.config/nvim/init.lua
+++ common-home/.config/nvim/init.lua
@@ -666,6 +666,32 @@ local plugins = {
         }
       }
     end
+  },
+  -- }}}
+  -- nvim-treesitter-objects {{{
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    config = function()
+      require('nvim-tmux-navigation').setup {
+        textobjects = {
+          select = {
+            enable = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              -- You can optionally set descriptions to the mappings (used in the desc parameter of
+              -- nvim_buf_set_keymap) which plugins like which-key display
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              -- You can also use captures from other query groups like `locals.scm`
+              ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+            },
+            include_surrounding_whitespace = true,
+          },
+        },
+      }
+    end
   }
   -- }}}
 }
