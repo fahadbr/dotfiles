ce00f3c2e85c533db6837a6bd2c606773eb80f88
Fahad Riaz <fahadbr@gmail.com>
2025-04-22 12:47:38 -0400

    formatting fine tuning

--- common-home/.config/nvim/lua/plugins/conform.lua
+++ common-home/.config/nvim/lua/plugins/conform.lua
@@ -2,37 +2,48 @@
 return {
   -- conform formatter  {{{
   {
-    "stevearc/conform.nvim",
+    'stevearc/conform.nvim',
     config = function()
       local conform = require('conform')
       conform.setup({
         formatters_by_ft = {
-          java = { "google-java-format" },
-          toml = { "toml" },
-          xml = { "xmllint" },
-          xsd = { "xmllint" },
-          json = { "jq" },
-          sql = { "pg_format" },
-          python = { "ruff_fix", "ruff_format", "ruff_organize_imports", "yapf" },
-          go = { "golines" },
-          ["_"] = { "trim_whitespace" },
+          java = { 'google-java-format' },
+          toml = { 'taplo' },
+          xml = { 'xmllint' },
+          xsd = { 'xmllint' },
+          json = { 'jq' },
+          sql = { 'pg_format' },
+          python = function()
+            if vim.fs.root(vim.env.PWD, '.style.yapf') ~= nil then
+              return { 'yapf' }
+            else
+              return { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' }
+            end
+          end,
+          go = { 'golines', 'gofumpt' },
+          lua = { 'stylua' },
+          ['_'] = { 'trim_whitespace' },
         },
         formatters = {
-          toml = {
-            command = "prettier",
-            args = { "--plugin", "prettier-plugin-toml", "$FILENAME" }
-          },
           xmllint = {
-            env = { XMLLINT_INDENT = "    " }
+            env = { XMLLINT_INDENT = '    ' },
           },
           golines = {
-            args = { "-m", "120" }
-          }
+            args = { '-m', '120' },
+          },
+          yapf = {
+            cwd = require('conform.util').root_file({ '.style.yapf' }),
+            require_cwd = true,
+          },
         },
       })
-      fr.nmap('<leader>fc', function() conform.format { lsp_fallback = true, timeout_ms = 1000 } end, "Format Using Conform")
-      fr.vmap('<leader>fc', function() conform.format { lsp_fallback = true, timeout_ms = 1000 } end, "Format Using Conform")
-    end
+      fr.nmap('<leader>fc', function()
+        conform.format({ lsp_fallback = true, timeout_ms = 1000 })
+      end, 'Format Using Conform')
+      fr.vmap('<leader>fc', function()
+        conform.format({ lsp_fallback = true, timeout_ms = 1000 })
+      end, 'Format Using Conform')
+    end,
   },
   -- }}}
 }
