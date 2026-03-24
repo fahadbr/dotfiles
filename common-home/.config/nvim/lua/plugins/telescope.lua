0cc87e35f95db2795773a62def159b43423f8d23
Fahad Riaz <fahadbr@gmail.com>
2026-03-24 13:46:04 -0400

    adding link following for grep

--- common-home/.config/nvim/lua/plugins/telescope.lua
+++ common-home/.config/nvim/lua/plugins/telescope.lua
@@ -19,6 +19,7 @@ return {
             '--with-filename',
             '--line-number',
             '--column',
+            '--follow',
             '--smart-case',
             '--hidden',
           },
