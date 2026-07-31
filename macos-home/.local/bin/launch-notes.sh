5977ca372ffc0796b282f8e24b0b4c833da05ead
Fahad Riaz <fahadbr@gmail.com>
2026-07-31 14:55:19 -0400

    more herdr migration

--- macos-home/.local/bin/launch-notes.sh
+++ macos-home/.local/bin/launch-notes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export KITTY_LISTEN_ON="unix:$(find ${TMPDIR} -name 'kitty*' 2>/dev/null | head -n 1)"
-kitten @launch --os-window-title=notes --type=os-window tmux attach -t notes
+# kitten @launch --os-window-title=notes --type=os-window tmux attach -t notes
+kitten @launch --os-window-title=notes --type=os-window herdr --session notes
 
